97b125b1aac50fa5609e213e7afe8fc0c38754cf
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-09-20 13:11:43 +0530

    Connection with mongodb completed || app-setup done

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,9 +1,37 @@
-const server = Bun.serve({
-    port: 3001,
-    fetch(req) {
-      return new Response("Bun!");
-    },
-  });
-  
-  console.log(`Listening on http://localhost:${server.port} ...`);
+import express from "express"
+import mongoose from "mongoose";
+
+const port = process.env.PORT
+
+const app=express()
+
+const connect=async()=>{
+
+    try{
+        await mongoose.connect(process.env.MONGO_URI!);
+        console.log("Connected to mongoDB")
+    }
+    catch(error){
+       
+        throw error
+    }
+    
+    }
+    
+// checking if mongodb is connected
+mongoose.connection.on("discconncted",()=>{
+    console.log('mongodb disconnected')
+
+})
+
+// checking if the connection is on
+mongoose.connection.on("connected",()=>{
+    console.log("MongoDB connected")
+})
+
+app.listen(port,()=>{
+    connect()
+    console.log("Connected to backend.")
+}) 
+console.log(`Listening on http://localhost:${port} ...`);
   
