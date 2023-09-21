575b0d196ec53b3c8a4b499162077da5e1a677e1
prateekmohanty63 <prateekmohanty63@gmail.com>
2023-09-21 12:55:26 +0530

    Feat:Chat refined session crud endpoint

--- Backend/routes/chat.route.ts
+++ Backend/routes/chat.route.ts
@@ -7,8 +7,8 @@ const router = express.Router();
 // CREAT SESSION
 
 router.post("/",creatNewSession)
-router.get("/sessionReq/:sessionId",getSessionById)
-router.get("/:userId",getAllChatSessions)
+router.get("/:sessionId",getSessionById)
+router.get("/user/:userId",getAllChatSessions)
 router.put("/:sessionId",createNewChatResponse)
 
 
